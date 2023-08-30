16843aa0543047ed447a4162f60115927024c835
KlaudjaKaraj <38klaudja.karaj@gmail.com>
2023-08-30 23:30:34 +0200

    adding greeting file

---
+++ greeting.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))  # Convert input to an integer
+
+# Calculate age next year
+age_next_year = age + 1
+
+# Print a greeting and age next year
+print(f"Hello, {name}! Next year, you will be {age_next_year} years old.")
